76972e52de76c6262d1a73cea37813226ad1610a
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-11-29 21:31:40 +0200

    fix homeImg

--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import HomeImage from '../../public/img/home.jpg';
 
 export const HomeGlobalStyle = createGlobalStyle`
 body{
     /* background-image: url('https://kartinki.pics/uploads/posts/2021-07/1625655366_42-kartinkin-com-p-biblioteka-art-art-krasivo-47.jpg'); */
 
-    background-image: url('../../public/img/home.jpg');
+    background-image: url(${HomeImage});
 }
 
 svg{
