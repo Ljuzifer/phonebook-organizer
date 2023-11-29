e1ab4d850e9d367ba6715b83871cf5f419c91f34
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-11-29 20:30:20 +0200

    fix imgs

--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -2,7 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 export const HomeGlobalStyle = createGlobalStyle`
 body{
-    background-image: url('https://kartinki.pics/uploads/posts/2021-07/1625655366_42-kartinkin-com-p-biblioteka-art-art-krasivo-47.jpg');
+    /* background-image: url('https://kartinki.pics/uploads/posts/2021-07/1625655366_42-kartinkin-com-p-biblioteka-art-art-krasivo-47.jpg'); */
+
+    background-image: url('../../public/img/home.jpg');
 }
 
 svg{
